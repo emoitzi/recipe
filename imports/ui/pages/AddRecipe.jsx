emoitzi/recipe

commit 7cf737725287f0d4d509c0388488c7db7dea5508

Use PropTypes from prop-types package

imports/ui/pages/AddRecipe.jsx
@@ -4,7 +4,6 @@ import {browserHistory} from 'react-router';
 import { ValidationError } from 'meteor/mdg:validation-error';
 
 
-import EditIngredients  from '../components/EditIngredients';
 import CategoryContainer from '../components/Category';
 import TextRecipe from '../components/TextRecipe';
 import PhotoRecipe from '../components/PhotoRecipe';

imports/ui/AdminApp.jsx
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes } from 'react';
+import React, {Component } from 'react';
+import PropTypes from 'prop-types';
 import {createContainer } from 'meteor/react-meteor-data';
 
 import { Categories } from '../api/categories.js'

imports/ui/AuthenticatedApp.jsx
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes } from 'react';
+import React, {Component } from 'react';
+import PropTypes from 'prop-types';
 import {createContainer } from 'meteor/react-meteor-data';
 import {browserHistory} from 'react-router';
 

imports/ui/components/Category.jsx
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor'
-import React, {Component, PropTypes } from 'react';
+import React, {Component } from 'react';
+import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import  { Categories } from '../../api/categories.js'

imports/ui/components/Ingredients.jsx
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes } from 'react';
+import React, {Component } from 'react';
+import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 

imports/ui/components/RecipeListItems.jsx
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes } from 'react';
+import React, {Component } from 'react';
+import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { Link } from 'react-router';

imports/ui/pages/ChangePwd.jsx
@@ -1,4 +1,4 @@
-import React, {Component, PropTypes } from 'react';
+import React, {Component } from 'react';
 import ReactDOM from 'react-dom';
 import  Blaze from 'meteor/gadicc:blaze-react-component';
 

imports/ui/pages/Recipe.jsx
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes } from 'react';
+import React, {Component  } from 'react';
+import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
 import {browserHistory} from 'react-router';
 

commit 544623bd33c22f8aad6154597173be35cfbb9d99

Fix required fields error ui broken in commit 7cf73772

.meteor/packages
@@ -8,7 +8,6 @@ meteor-base@1.0.4             # Packages every Meteor app needs to have
 mobile-experience@1.0.4       # Packages for a great mobile UX
 mongo@1.1.16                   # The database Meteor supports right now
 blaze-html-templates@1.0.4 # Compile .html files into Meteor Blaze views
-reactive-var@1.0.11            # Reactive variable for tracker
 jquery@1.11.10                  # Helpful client-side library
 tracker@1.1.2                 # Meteor's client-side reactive programming library
 

imports/ui/pages/AddRecipe.jsx
@@ -5,12 +5,13 @@ import {browserHistory} from 'react-router';
 
 import RecipeForm from './RecipeForm';
 
+
 export default AddRecipe = createContainer(() => {
-  let errors = {};
-  function handleSubmit (recipe) {
-    errors = {};
+  function handleSubmit (recipe, callback) {
     Meteor.call('recipes.insert', recipe, (err, id) => {
+      let errors;
       if (err) {
+        errors = {};
         err.details.forEach((fieldError) => {
           errors[fieldError.name] = true;
         });
@@ -19,11 +20,11 @@ export default AddRecipe = createContainer(() => {
         //success
         browserHistory.push('/recipe/' + id);
       }
+      callback(errors)
     });
   }
   return {
     onSubmit: handleSubmit,
-    errors: errors,
     ready: true,
   };
 

imports/ui/pages/EditRecipe.jsx
@@ -9,7 +9,6 @@ import { Images } from '../../api/images.js';
 
 export default EditRecipe = createContainer((params) => {
   Meteor.subscribe("recipes");
-  let errors = {};
   const recipe = Recipes.findOne({_id: params.params.id});
   let titleImage;
   let recipeImage;
@@ -24,10 +23,11 @@ export default EditRecipe = createContainer((params) => {
     ready = true;
   }
 
-  function handleSubmit (recipe) {
-    errors = {};
+  function handleSubmit (recipe, callback) {
+    let errors;
     Meteor.call('recipes.update', recipe._id, recipe, (err) => {
       if (err) {
+        errors = {};
         err.details.forEach((fieldError) => {
           errors[fieldError.name] = true;
         });
@@ -36,6 +36,7 @@ export default EditRecipe = createContainer((params) => {
         //success
         browserHistory.push('/recipe/' + recipe._id);
       }
+      callback(errors);
     });
   }
   return {
@@ -43,7 +44,6 @@ export default EditRecipe = createContainer((params) => {
     titlePreviewSrc: titleImage && titleImage.link(),
     recipePreviewSrc: recipeImage && recipeImage.link(),
     onSubmit: handleSubmit,
-    errors: errors,
     ready: ready,
     title: "Rezept ändern",
   };

imports/ui/pages/RecipeForm.jsx
@@ -28,7 +28,7 @@ export default class RecipeForm extends Component {
       recipePreviewSrc: props.recipePreviewSrc || '',
       titlePreviewSrc: props.titlePreviewSrc || '',
       uploadingImage: 0,
-      errors: props.errors,
+      errors: {},
     };
   }
 
@@ -37,7 +37,7 @@ export default class RecipeForm extends Component {
       recipe : props.recipe || this.state.recipe,
       recipePreviewSrc: props.recipePreviewSrc || '',
       titlePreviewSrc: props.titlePreviewSrc || '',
-      errors: props.errors,
+
     };
     this.setState(state);
   }
@@ -98,7 +98,12 @@ export default class RecipeForm extends Component {
 
   handleSubmit (event) {
     event.preventDefault();
-    this.props.onSubmit(this.state.recipe);
+    let self = this;
+    this.props.onSubmit(this.state.recipe, (err) => {
+      if (err) {
+        self.setState({"errors": err});
+      }
+    });
 
   }
   handleImageChange(file, resolution, idStateKey, srcStateKey) {
@@ -311,7 +316,6 @@ export default class RecipeForm extends Component {
 
 RecipeForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-  errors: PropTypes.object,
   recipe: PropTypes.object,
   recipePreviewSrc: PropTypes.string,
   titlePreviewSrc: PropTypes.string,